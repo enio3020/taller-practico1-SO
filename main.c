4d075b13b173c189e4818a9ac0a0d55ed80dfc7e
enio3020 <enioarmandohernandez@gmail.com>
2020-10-15 16:55:03 -0500

    primero commit

---
+++ main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/shm.h>
+#include <wait.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define LIM_ROWS 10
+#define LIM_COLS 10
+
+int main(int argc, char const *argv[]){
+
+    
+    return 0;
+}
